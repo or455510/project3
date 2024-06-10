3c8f3389155e375a9d5b0ddf5eb17b250e2fba51
omarRamadan <158860126+or455510@users.noreply.github.com>
2024-06-10 12:02:26 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,338 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header >
+        <
+        a href = "#"
+        className = "logo" > Black Horse < /a> <
+        nav className = "navegashen" >
+        <
+        a href = "#Drink" > Drink < /a> <
+        a href = "#VIP" > VIP < /a> <
+        a href = "#malek" > malek < /a> <
+        a href = "#Teem" > Teem < /a> <
+        /nav> <
+        /header> <
+        section className = "myprofile" >
+        <
+        p > Hallo
+        for Black Horse Cafe < /p> <
+        h2 > the best cafe
+        for banha < /h2> <
+        a href = "#" > viow Drink < /a>
+
+        <
+        /section> { /* ============================== */ }
+
+        <
+        div >
+
+        <
+        section className = "nametitel "
+        id = "#Drink " >
+        <
+        h2 className = "tittel " > Drink < /h2> <
+        /section> <
+        section className = "card "
+        id = "services " >
+        <
+        div className = "cards " >
+        <
+        img className = "imgs "
+        src = "FB_IMG_1704826405302.jpg " / >
+        <
+        h3 > moka < /h3> <
+        /div> <
+        div className = "cards " >
+        <
+        img src = " FB_IMG_1704826521788.jpg " / >
+        <
+        h3 > cahlab < /h3> <
+        /div> <
+        div className = "cards " >
+        <
+        img src = "FB_IMG_1704826749812.jpg " / >
+        <
+        h3 > wafel < /h3> <
+        /div> <
+        div className = "cards " >
+        <
+        img src = " FB_IMG_1704826521788.jpg " / >
+        <
+        h3 > ice orio < /h3> <
+        /div> <
+        div className = "cards " >
+        <
+        img src = "FB_IMG_1704826405302.jpg " / >
+        <
+        h3 > wafel & amp; mango < /h3> <
+        /div> <
+        div className = "cards " >
+        <
+        img src = "FB_IMG_1704826749812.jpg " / >
+        <
+        h3 > moka < /h3> <
+        /div> <
+        div className = "cards " >
+        <
+        img src = " FB_IMG_1704826521788.jpg " / >
+        <
+        h3 > ice orio < /h3> <
+        /div> <
+        div className = "cards " >
+        <
+        img src = "FB_IMG_1704826405302.jpg " / >
+        <
+        h3 > wafel & amp; mango < /h3> <
+        /div> <
+        /section> <
+        section className = "namevip " >
+        <
+        h2 className = "titelvip " > vip < /h2> <
+        /section> <
+        section className = "vip "
+        id = "VIP " >
+        <
+        div className = "cardsvip " >
+        <
+        i className = "fas fa-edit " / >
+        <
+        h3 > vip < /h3> <
+        img src = " FB_IMG_1704826295936.jpg "
+        width = { 200 }
+        /> <
+        p > Lorem ipsum, dolor sit amet consectetur adipisicing elit.Accusantium rerum quia nostrum possimus dolorem sed obcaecati voluptates.Voluptatibus dolorum a distincti < /p> <
+        /div> <
+        div className = "cardsvip " >
+        <
+        i className = "fas fa-edit " / >
+        <
+        img src = " FB_IMG_1704826710111.jpg "
+        width = { 200 }
+        /> <
+        h3 > vip < /h3> <
+        p > Lorem ipsum, dolor sit amet consectetur adipisicing elit.Accusantium rerum quia nostrum possimus dolorem sed obcaecati voluptates.Voluptatibus dolorum a distincti < /p> <
+        /div> <
+        div className = "cardsvip " >
+        <
+        i className = "fas fa-edit " / >
+        <
+        img src = "FB_IMG_1704826302503.jpg "
+        width = { 150 }
+        height = { 150 }
+        /> <
+        h3 > barth day < /h3> <
+        p > Lorem ipsum, dolor sit amet consectetur adipisicing elit.Accusantium rerum quia nostrum possimus dolorem sed obcaecati voluptates.Voluptatibus dolorum a distincti < /p> <
+        /div> <
+        /section> <
+        section className = "vip2 "
+        id = "myproject " >
+        <
+        div >
+        <
+        h2 className = "h22 " > VIP < /h2> <
+        p > لو عاوز تعمل عيد ميلادك🎂 او خطوبه💝 ومحتار في مكان يقدم لك خدمه ممتازه وتبقي قاعد قاعده حلوه في مكان شيك انت وكل ضيوفك؟ تقدر عندنا تعمل عيد ميلادك او خطوبتك في مكان منفصل تماما عن باقي جيست الكافيه يعني خصوصيه كامله ليك ولكل ضيوفك😉😊 ابعت لنا رساله او اكتب لنا كومنت وهنبعت لك تفاصيل اعياد الميلاد او الخطوبه او اعياد ميلاد الاطفال والسبوع🥰🤩 ولو حابب تستفسر عن اي حاجه اكتب لنا كومنت او ابعت لنا رساله وهنرد عليك ف اسرع وقت -
+        نتشرف بخدمتكم دائما🤩🤩🤩📌 ع <
+        /p> <
+        a href = "www.whatsapp.com "
+        className = "whats " > Whatsapp: 01143458131 < /a> <
+        /div> <
+        div className = "notilasora " >
+        <
+        video controls autoPlay loop muted width = "500px "
+        height = { 400 } >
+        <
+        source src = "Facebook_1682201181232.mp4 "
+        type = "video.mp4 " / >
+        <
+        source src = "Facebook_1682201181232.ogg "
+        type = "video.ogg " / >
+        <
+        /video></div >
+        <
+        /section> <
+        section className = "nameoner " >
+        <
+        h2 className = "oner " > malek < /h2> <
+        /section> <
+        section className = "malek "
+        id = "#malek " >
+        <
+        div className = "ProjectCard " >
+        <
+        div className = "Projectimg " >
+        <
+        img src = "IMG_٢٠٢٤٠١١٠_١٦٢٢٢٤.jpg "
+        width = { 200 }
+        /> <
+        /div> <
+        div className = "Projectinfo " >
+        <
+        p > Lorem ipsum dolor sit, amet consectetur < /p> <
+        strong className = "ptitel " >
+        <
+        span > MR.mostafa < /span> <
+        a href = "# "
+        className = "more " > facebook < /a> <
+        /strong> <
+        /div> <
+        /div> <
+        div className = "ProjectCard " >
+        <
+        div className = "Projectimg " >
+        <
+        img src = "IMG_٢٠٢٤٠١١٠_١٦٢٢١٠.jpg "
+        width = { 200 }
+        /> <
+        /div> <
+        div className = "Projectinfo " >
+        <
+        p > Lorem ipsum dolor sit, amet consectetur < /p> <
+        strong className = "ptitel " >
+        <
+        span > Amir < /span> <
+        a href = "https://www.facebook.com/amir.abdelatty.31?mibextid=9R9pXO "
+        className = "more " > facebook < /a> <
+        /strong> <
+        /div> <
+        /div> <
+        div className = "ProjectCard " >
+        <
+        div className = "Projectimg " >
+        <
+        img src = " FB_IMG_1704826803438.jpg "
+        width = { 200 }
+        /> <
+        /div> <
+        div className = "Projectinfo " >
+        <
+        p > Lorem ipsum dolor sit, amet consectetur < /p> <
+        strong className = "ptitel " >
+        <
+        span > adham < /span> <
+        a href = "https://www.facebook.com/profile.php?id=100009049986319&mibextid=9R9pXO "
+        className = "more " > facebook < /a> <
+        /strong> <
+        /div> <
+        /div> <
+        /section> <
+        section className = "team " >
+        <
+        h2 > Team < /h2> <
+        /section> <
+        section className = "cardteam "
+        id = "#Teem " >
+        <
+        div className = "cardt " >
+        <
+        div className = "teamimg " >
+        <
+        img src = "../tikt/عمار.webp " / >
+        <
+        div className = "teaminfo " >
+        <
+        p > 3 andaleeb < /p> <
+        a href = "https://www.facebook.com/omarmezo.amr?mibextid=9R9pXO " > facebook < /a> <
+        /div> <
+        /div> <
+        /div> <
+        div className = "cardt " >
+        <
+        div className = "teamimg " >
+        <
+        img src = "FB_IMG_1704827034406.jpg " / >
+        <
+        div className = "teaminfo " >
+        <
+        p > Zezo < /p> <
+        a href = " https://www.facebook.com/profile.php?id=100011864887334&mibextid=9R9pXO " > facebook < /a> <
+        /div> <
+        /div> <
+        /div> <
+        div className = "cardt " >
+        <
+        div className = "teamimg " >
+        <
+        img src = "FB_IMG_1704826989921.jpg " / >
+        <
+        div className = "teaminfo " >
+        <
+        p > Adham < /p> <
+        a href = "https://www.facebook.com/profile.php?id=100009466987308&mibextid=9R9pXO " > facebook < /a> <
+        /div> <
+        /div> <
+        /div> <
+        div className = "cardt " >
+        <
+        div className = "teamimg " >
+        <
+        img src = "FB_IMG_1704826925801.jpg " / >
+        <
+        div className = "teaminfo " >
+        <
+        p > Mostafa Desha < /p> <
+        a href = "https://www.facebook.com/profile.php?id=100018548104349&mibextid=9R9pXO " > facebook < /a> <
+        /div> <
+        /div> <
+        /div> <
+        div className = "cardt " >
+        <
+        div className = "teamimg " >
+        <
+        img src = "FB_IMG_1704827055693.jpg " / >
+        <
+        div className = "teaminfo " >
+        <
+        p > 3 raby < /p> <
+        a href = "https://www.facebook.com/profile.php?id=100028352580097&mibextid=9R9pXO " > facebook < /a> <
+        /div> <
+        /div> <
+        /div> <
+        /section> <
+        section className = "loc " >
+        <
+        div className = "locashin " >
+        <
+        p >: منشيه النور شارع ترعة الإشارة بجوار مباحث الكهرباء < /p>     <
+        /div> <
+        div className = "imges " >
+        <
+        iframe className = "map "
+        src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3439.3805880026575!2d31.187004599999998!3d30.453656500000008!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1458759c2e2c47c1%3A0xfc86b12bc5335eb!2sBlack%20horse%20cafe!5e0!3m2!1sen!2seg!4v1706095994492!5m2!1sen!2seg
+        " width={600 } height={450 } style={{border: 0}} allowFullScreen="
+        " loading="
+        lazy " referrerPolicy="
+        no - referrer - when - downgrade " />  <
+        a href = "https://maps.app.goo.gl/nRqc47HcxSNE93SS8 " > locashin < /a> { /* <img src="IMG_٢٠٢٤٠١٢١_١٤٤٢٢٥.jpg "> */ } <
+        /div> <
+        /section> <
+        footer className = "foot " >
+        <
+        p > Black Horse Cafe < /p> <
+        ul className = "footicons " >
+        <
+        a href = "./webfonts/fa-brands-400.eot "
+        className = "icon brands fa-twitter " > < span className = "label " > Twitter < /span></a >
+        <
+        a href = "./webfonts/fa-regular-400.woff "
+        className = "icon brands fa-facebook-f " > < span className = "label " > Facebook < /span></a >
+        <
+        a href = "# "
+        className = "icon brands fa-instagram " > < span className = "label " > Instagram < /span></a >
+        <
+        /ul> <
+        /footer> <
+        /div> <
+        /div>
+    );
+}
+// function secthon(){
+//   return(
+
+//   );
+// }
+export default App;
